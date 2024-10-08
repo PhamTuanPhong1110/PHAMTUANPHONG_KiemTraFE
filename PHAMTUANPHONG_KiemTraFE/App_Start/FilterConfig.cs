6d2d5b3145af890caa7cf7b4e25b1fd5d04c55c8
ASUS Vivobook <ASUS Vivobook@LAPTOP-RA51567G>
2024-10-08 15:10:37 +0700

    Add project files.

---
+++ PHAMTUANPHONG_KiemTraFE/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PHAMTUANPHONG_KiemTraFE
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
